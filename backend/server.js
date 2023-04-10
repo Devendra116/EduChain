218ff9430a7ae2b475af0fce2024e1e2cc18ef8b
Devendra116 <chauhandevendra116@gmail.com>
2023-04-10 23:41:16 +0530

    Initial Project Setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db')
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.listen(process.env.PORT, () =>
+    connectDB()
+        .then(() => console.log(`Server is up and running at http://localhost:${process.env.PORT}`))
+        .catch(() =>
+            console.log('Server is running , but database connection failed')
+        )
+);
