8375b1dd4ed901456a8b85cec0b6d976bd80b3df
Devendra116 <chauhandevendra116@gmail.com>
2023-04-17 12:25:56 +0530

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ const express = require('express')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db')
+const cors = require('cors')
 
 
 const app = express()
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/user', require('./routes/user'))
 app.use('/ngo', require('./routes/ngo'))
+app.use('/course', require('./routes/course'))
 
 
 app.listen(process.env.PORT, () =>
